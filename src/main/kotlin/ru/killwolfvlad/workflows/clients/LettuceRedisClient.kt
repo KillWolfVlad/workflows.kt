7749f53ab67844d2cf6d26ea7196658c80d2aa30
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2025-02-01 19:39:14 +0500

    fix: mark lettuce as deprecated

--- src/main/kotlin/ru/killwolfvlad/workflows/clients/LettuceRedisClient.kt
+++ src/main/kotlin/ru/killwolfvlad/workflows/clients/LettuceRedisClient.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.reactive.awaitFirstOrNull
 import ru.killwolfvlad.workflows.core.annotations.WorkflowsPerformance
 import ru.killwolfvlad.workflows.core.interfaces.KeyValueClient
 
+// TODO: delete Lettuce client
 @OptIn(WorkflowsPerformance::class, ExperimentalLettuceCoroutinesApi::class)
 class LettuceRedisClient(
     rootJob: Job,
